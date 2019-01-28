32935a32a0aacbf5654f0f4f0cd612a52ed09808
Kapil Das <kapil.das.etc@gmail.com>
2019-01-28 00:43:42 -0500

    Use StringUtils and added denedencies in POM xml

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -3,7 +3,18 @@ package clinic.programming.training;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+	
+	public Application() {
+        System.out.println ("Inside Application");
+    }
+	
+	private int countWords(String words) {
+		String[] separatedWords = StringUtils.split(words, " ");
+		return (separatedWords == null) ? 0: separatedWords.length;
+	}
 	
 	public void greet() {
 		List<String> greetings = new ArrayList<>();
@@ -13,15 +24,14 @@ public class Application {
 			System.out.println ("Greeting: " + greeting);
 		}
 	}
-    
-    public Application() {
-        System.out.println ("Inside Application");
-    }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		
+		int count = app.countWords("I have four words");
+		System.out.println("Word count: " + count);
     }
 }
